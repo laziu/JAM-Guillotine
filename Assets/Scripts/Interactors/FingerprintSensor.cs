883663992127480027fe4f6028c63aac9cc1ea68
15박보승 <pbs04054@gmail.com>
2019-08-16 23:39:08 +0900

    머리 파트 컨트롤러 작업

--- Assets/Scripts/Interactors/FingerprintSensor.cs
+++ Assets/Scripts/Interactors/FingerprintSensor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class FingerprintSensor : IBodyInteractor
+public class FingerprintSensor : MonoBehaviour, IBodyInteractor
 {
 	public UnityAction OnInteract;
 
