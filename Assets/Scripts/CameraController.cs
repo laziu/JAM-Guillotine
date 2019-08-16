63df073e74fdd4c9f1997b2e540631fe662fef53
15박보승 <pbs04054@gmail.com>
2019-08-16 22:31:40 +0900

    간단한 카메라 컨트롤러 추가

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+	[SerializeField]
+	private Transform target;
+	private float offsetZ;
 
+	private void Start()
+	{
+		offsetZ = transform.position.z - target.position.z;
+	}
+
+	private void LateUpdate()
+	{
+		transform.position = Vector3.Lerp(transform.position, target.position + new Vector3(0, 0, offsetZ), 0.8f);
+	}
 }
