1f3609c5cf22fa0ba534ac23e4b5938794ecc9f1
laziu <laziu.cc@gmail.com>
2019-08-17 03:48:36 +0900

    문 구현

---
+++ Assets/Scripts/ButtonAdapter.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ButtonAdapter : MonoBehaviour
+{
+    public abstract void Signal();
+}
---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public ButtonAdapter[] triggerList;
+
+    public void Interact()
+    {
+        foreach (var i in triggerList)
+            i.Signal();
+    }
+}
