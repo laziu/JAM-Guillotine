38b726db05af362f3fa288d9015e3fa68259af1f
laziu <laziu.cc@gmail.com>
2019-08-16 22:53:10 +0900

    BodyController 구현

--- Assets/Scripts/BodyController.cs
+++ Assets/Scripts/BodyController.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class BodyController : MonoBehaviour
 {
+    public float runSpeed = 10f;
+    public KeyCode m_left = KeyCode.A;
+    public KeyCode m_right = KeyCode.D;
+    public KeyCode m_jump = KeyCode.Space;
+    public KeyCode m_crouch = KeyCode.C;
 
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    private Movement2D mv;
+
+    private void Start()
+    {
+        mv = GetComponent<Movement2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalMove = (Input.GetKey(m_right) ? runSpeed : 0) 
+                             - (Input.GetKey(m_left) ? runSpeed : 0);
+        bool jump = Input.GetKeyDown(m_jump);
+        bool crouch = Input.GetKey(m_crouch);
+        mv.Move(horizontalMove * runSpeed * Time.fixedDeltaTime, crouch, jump);
+    }
 }
