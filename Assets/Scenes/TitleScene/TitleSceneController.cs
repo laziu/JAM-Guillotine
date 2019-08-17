a9108226ee60a895a4c7f511aac160fa40ffdca5
laziu <laziu.cc@gmail.com>
2019-08-18 05:08:17 +0900

    씬 넘기기 구현

---
+++ Assets/Scenes/TitleScene/TitleSceneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public TimelineAsset timeline;
+    public new Transform camera;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            playableDirector.Play(timeline);
+    }
+
+    public void StartGame()
+    {
+        playableDirector.Play(timeline);
+        StartCoroutine(FinishTimeline());
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator FinishTimeline()
+    {
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < 1000; i++)
+        {
+            camera.position += new Vector3(.02f * i, .02f * i, -.02f * i);
+            yield return i;
+        }
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Scenes/Stage1");
+    }
+}
