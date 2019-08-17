aaa4624f8927513de7616f25f4a87c828b54efdb
laziu <laziu.cc@gmail.com>
2019-08-17 15:35:26 +0900

    LegoTrap, DeadTrap 구현

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public enum TrapType { Lego, Dead };
+    public TrapType trapType;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Traped " + trapType);
+        switch(trapType)
+        {
+            case TrapType.Lego: collision.gameObject.GetComponent<Actor>().GetDamaged(1); break;
+            case TrapType.Dead: collision.gameObject.GetComponent<Actor>().GetDamaged(99999); break;
+        }
+    }
+}
