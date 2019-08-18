108682630f32aa8d1a5087b8c9ac75f419799924
laziu <laziu.cc@gmail.com>
2019-08-18 12:12:38 +0900

    스테이지 3-1까지 연결

--- Assets/Scripts/BodyController.cs
+++ Assets/Scripts/BodyController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class BodyController : MonoBehaviour
 {
     [SerializeField] private float maxSpeed = 10f;
